33d158c889d4ab7a62f94f295febcb54688dfcb8
Haynie <yoitzgametime@gmail.com>
2025-02-01 20:54:54 -0600

    NOT FINISHED NEEDS ART AND LINKING

---
+++ signin/signin.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const signInBtn = document.querySelector(".sign-in");
+    const createAccountBtn = document.querySelector(".create-account");
+
+    signInBtn.addEventListener("click", function() {
+        const username = document.querySelector("input[type='text']").value;
+        const password = document.querySelector("input[type='password']").value;
+        
+        if (!username || !password) {
+            alert("Please enter both username and password.");
+            return;
+        }
+        
+        console.log("User signed in:", { username, password });
+        alert("Sign In Successful!");
+    });
+
+    createAccountBtn.addEventListener("click", function() {
+        window.location.href = "create.html"; // Redirect to the create account page
+    });
+});
