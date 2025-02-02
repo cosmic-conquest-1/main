a29c3a593422b18653d99bb4c8f09254b52c1b02
Haynie <yoitzgametime@gmail.com>
2025-02-01 22:07:40 -0600

    Latest and greatest

---
+++ frontend_js/titlescreen.js
@@ -0,0 +1,24 @@
+function startGame() {
+    alert("Starting the game...");
+    window.location.href = "game.html"; // Redirect to the game page
+}
+
+function openLeaderboard() {
+    alert("Opening leaderboard...");
+    window.location.href = "leaderboard.html";
+}
+
+function openTutorial() {
+    alert("Opening tutorial...");
+    window.location.href = "tutorial.html";
+}
+
+function openOptions() {
+    alert("Opening options...");
+    window.location.href = "options.html";
+}
+
+function quitGame() {
+    alert("Quitting game...");
+    window.close(); // Attempts to close the window (may not work in all browsers)
+}
